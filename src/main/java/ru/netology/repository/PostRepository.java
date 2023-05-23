1abb83578b4b978bf8a41d430df0b43082335ebd
Tatiana <in_sdo@mail.ru>
2023-05-23 14:47:13 +0300

    Annotation Config

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.Collection;
@@ -10,7 +11,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
+@Repository
 public class PostRepository {
     private final ConcurrentMap<Long, Post> mapRepo;
     private final AtomicLong idCounter = new AtomicLong();
